4b163d51e60ca7ed604f45245d75eabbfeebacd7
Sergey Chikuyonok <serge.che@gmail.com>
2016-11-18 12:41:55 +0300

    🌟Initial implmentation of content pasting

--- test/repeater-content.js
+++ test/repeater-content.js
@@ -3,8 +3,13 @@
 const assert = require('assert');
 const parse = require('@emmetio/abbreviation');
 require('babel-register');
-const find = require('../lib/repeater-content').findPlaceholders;
+const insert = require('../lib/repeater-content').default;
+const stringify = require('./assets/stringify').default;
 
 describe('Repeater content', () => {
-    
+	const expand = (abbr, content) => stringify( insert(parse(abbr), content) );
+
+	it('implicit repeat', () => {
+		assert.equal(expand('ul>li*', ['foo', 'bar']), '<ul><li*2@1>foo</li><li*2@2>bar</li></ul>');
+	});
 });
