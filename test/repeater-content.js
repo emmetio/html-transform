27f450d84d877aabffa1cba3df1fd61940e22a7c
Sergey Chikuyonok <serge.che@gmail.com>
2016-11-18 23:34:12 +0300

    🌟Test coverage for Reapeter Content module

--- test/repeater-content.js
+++ test/repeater-content.js
@@ -13,4 +13,22 @@ describe('Repeater content', () => {
 		assert.equal(expand('ul>li*', ['foo', 'bar']), '<ul><li>foo</li><li>bar</li></ul>');
 		assert.equal(expand('ul>li*>a+b', ['foo', 'bar']), '<ul><li><a></a><b>foo</b></li><li><a></a><b>bar</b></li></ul>');
 	});
+
+    it('no repeat', () => {
+        assert.equal(expand('ul>li', ['foo', 'bar']), '<ul><li>foo\nbar</li></ul>');
+        assert.equal(expand('ul>li', 'foo'), '<ul><li>foo</li></ul>');
+    });
+
+    it('output placeholder', () => {
+        assert.equal(expand('ul>li[class=$#]*', ['foo', 'bar']), '<ul><li class="foo"></li><li class="bar"></li></ul>');
+        assert.equal(expand('ul>li[class=$#]*{content: $#}', ['foo', 'bar']), '<ul><li class="foo">content: foo</li><li class="bar">content: bar</li></ul>');
+        assert.equal(expand('ul>li[class=$#]*>a+b{content: $#}', ['foo', 'bar']), '<ul><li class="foo"><a></a><b>content: foo</b></li><li class="bar"><a></a><b>content: bar</b></li></ul>');
+        assert.equal(expand('ul>li*>a+b{content: $#}', ['foo', 'bar']), '<ul><li><a></a><b>content: foo</b></li><li><a></a><b>content: bar</b></li></ul>');
+    });
+
+    it('URL patterns', () => {
+        assert.equal(expand('p*>a', ['http://emmet.io', 'https://livestyle.io']), '<p><a href="http://emmet.io">http://emmet.io</a></p><p><a href="https://livestyle.io">https://livestyle.io</a></p>');
+        assert.equal(expand('p*>a', ['emmet.io/foo', 'livestyle.io/bar/baz/']), '<p><a href="http://emmet.io/foo">emmet.io/foo</a></p><p><a href="http://livestyle.io/bar/baz/">livestyle.io/bar/baz/</a></p>');
+        assert.equal(expand('p*>a', 'info@emmet.io'), '<p><a href="mailto:info@emmet.io">info@emmet.io</a></p>');
+    });
 });
