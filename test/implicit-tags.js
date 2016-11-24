3647af6870420d21e124a3faecd4735aee9b2618
Sergey Chikuyonok <serge.che@gmail.com>
2016-11-25 00:56:13 +0300

    ⚠️ Do not add tag name to text-only nodes

--- test/implicit-tags.js
+++ test/implicit-tags.js
@@ -24,5 +24,7 @@ describe('Implicit tags', () => {
         assert.equal(expand('p.foo>.bar'), '<p class="foo"><span class="bar"></span></p>');
         assert.equal(expand('ul>.item*2'), '<ul><li*2@1 class="item"></li><li*2@2 class="item"></li></ul>');
         assert.equal(expand('table>.row>.cell'), '<table><tr class="row"><td class="cell"></td></tr></table>');
+        assert.equal(expand('{test}'), 'test');
+        assert.equal(expand('.{test}'), '<div class="">test</div>');
     });
 });
