f4b1a048791456b5239c4e1652f981928a31fdbf
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-09 12:12:41 +0300

    🎈XSL Transformer

---
+++ test/xsl.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const assert = require('assert');
+const parse = require('@emmetio/abbreviation');
+require('babel-register');
+const xsl = require('../lib/xsl').default;
+const stringify = require('./assets/stringify').default;
+
+describe('XSL transform', () => {
+	const expand = abbr => stringify( xsl( parse(abbr) ) );
+
+    it('transform', () => {
+		assert.equal(expand('xsl:variable[select]'), '<xsl:variable select=""></xsl:variable>');
+		assert.equal(expand('xsl:with-param[select]'), '<xsl:with-param select=""></xsl:with-param>');
+
+		assert.equal(expand('xsl:variable[select]>div'), '<xsl:variable><div></div></xsl:variable>');
+		assert.equal(expand('xsl:with-param[select]{foo}'), '<xsl:with-param>foo</xsl:with-param>');
+    });
+});
